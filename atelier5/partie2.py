a64bd71632b57901df72749c5bd0f44380c9f3ac
Abdou-salam <dialloabdousalam20@gmail.com>
2023-09-25 15:28:55 +0200

    atelier5

--- atelier5/partie2.py
+++ atelier5/partie2.py
@@ -2,13 +2,12 @@ import numpy as np
 #Question
 def my_searchsorted(table:object,indice:int)->int:
     i = 0
-    trouve = True
-    while  table[i]!= indice   and trouve  :
+    
+    while  table[i]!= indice   and i != -1  :
         if i < len(table) - 1:
             i += 1
         else:
             i = -1
-            trouve = False
     return i
 arr = np.array([5,9,7,34,71,0,8])
 print("L'indice de la valeur rechercher est:",my_searchsorted(arr,34))
@@ -24,10 +23,13 @@ def my_where(table:object,valeur:int)->list:
         if valeur == table[index]:
             res.append(index)
     return res
+
 print("fonction where")
 arr2 = np.array([5,9,7,34,9,71,9,0,8])
 print("Les indices de la valeur rechercher est:",my_where(arr2,9))
 
+
+print("  ")
 #Question3
 
 def my_add(tableA:object,tableB:object)->object:
@@ -85,3 +87,59 @@ def matrice_trace(matrice:object):
 A = np.array(([3,1],[6,4]))
 print("fonction matrice_trace")
 print(matrice_trace(A))
+
+print("fonction est symetrie")
+def est_symetrique(matrice:object)->bool:
+    ligne = matrice.ndim
+    colonne = len(matrice)
+    res = False
+    for index_l in range(ligne):
+        for index_col in range(colonne):
+            if matrice[index_l,index_col] != matrice[index_col,index_l]:
+                res = False
+                break
+            else:
+                res = True
+    return res
+
+A = np.array(([3,1],[6,4]))
+B = np.array(([1,8],[8,1]))
+print("Matrice:",A,"\t",est_symetrique(A))
+print("Matrice:",B,"\t",est_symetrique(B))
+
+#Methode 2
+#def est_symetrique2(matrice:object)->bool:
+ #   ligne = matrice.ndim - 1
+  #  colonne = len(matrice) - 1
+   # index_col = 0
+    #index_l  = 0
+    #res =True
+    #while index_l != ligne and res:
+     #   while index_col != colonne and res  :
+      #      if matrice[index_l,index_col] == matrice[index_col,index_l]:  
+              #    index_col += 1
+              #    res =True
+         #     else:
+           #       res = False
+                
+      #    index_l  += 1        
+   #   return res
+#  c = np.array(([5,0],[8,4]))
+#  d = np.array(([2,9],[9,2]))
+#  print("meyhode2 fonction est_semetrique")
+#  print("Matrice:",c,"\t",est_symetrique2(c))
+#  print("Matrice:",d,"\t",est_symetrique2(d))
+
+
+def produit_diagonal(matrice:object)->int:
+    ligne = matrice.shape[0]
+    colonne = matrice.shape[1]
+    produit = 1
+    for index_l in range(ligne):
+        for index_col in range(colonne):
+            if index_l == index_col:
+                produit *= matrice[index_l,index_col]
+    return produit
+print("produit diagonal")
+A = np.array(([3,1],[6,4]))
+print(produit_diagonal(A))
